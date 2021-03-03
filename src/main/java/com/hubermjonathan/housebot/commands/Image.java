e00e98cdbf4cf5d5a2fe7562911e7e670982bc52
Jon Huber <hubermjonathan@gmail.com>
2021-03-03 18:00:30 -0500

    added link for images

--- src/main/java/com/hubermjonathan/housebot/commands/Image.java
+++ src/main/java/com/hubermjonathan/housebot/commands/Image.java
@@ -22,14 +22,20 @@ public class Image extends Command {
             }
         }
 
-        if (getEvent().getMessage().getAttachments().size() != 1 || !getEvent().getMessage().getAttachments().get(0).isImage()) {
+        if ((getEvent().getMessage().getAttachments().size() != 1 && getArgs().length == 0) || (getEvent().getMessage().getAttachments().size() == 1 && !getEvent().getMessage().getAttachments().get(0).isImage())) {
             getEvent().getMessage().addReaction(Constants.DENY).queue();
             throw new Exception();
         }
 
         Message roomMessage = category.getTextChannels().get(0).retrievePinnedMessages().complete().get(0);
         EmbedBuilder embedBuilder = new EmbedBuilder(roomMessage.getEmbeds().get(0));
-        embedBuilder.setImage(getEvent().getMessage().getAttachments().get(0).getUrl());
+
+        if (getArgs().length == 1) {
+            embedBuilder.setImage(getArgs()[0]);
+        } else {
+            embedBuilder.setImage(getEvent().getMessage().getAttachments().get(0).getUrl());
+        }
+
         roomMessage.editMessage(embedBuilder.build()).queue();
     }
 }
